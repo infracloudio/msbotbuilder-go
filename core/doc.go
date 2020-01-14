3001758d94d4f41f3eb9052cc1c4a7fb2ff5f8e5
sahil-lakhwani <sahilakhwani@gmail.com>
2020-01-14 17:04:46 +0530

    Add example for adapter

--- core/doc.go
+++ core/doc.go
@@ -1,5 +1,6 @@
 /*Package core is the entry point and the main interface for a user program.
 
 It provides an adapter to the user program using which all the operations can be made on this SDK.
+See the example to understand how to use this package to perform operations on the Bot Framwework connector service.
 */
 package core
