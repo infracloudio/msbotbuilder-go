0ecde73bb1a629176c7c3b55e26b7238887725a2
Prasad Ghangal <prasad.ghangal@gmail.com>
2023-11-29 16:43:33 +0530

    Fix gofmt issues
    Signed-off-by: Prasad Ghangal <prasad.ghangal@gmail.com>

--- samples/echobot/doc.go
+++ samples/echobot/doc.go
@@ -1,17 +1,18 @@
-/*Bot Framework echo bot sample.
+/*
+Bot Framework echo bot sample.
 This bot uses msbotbuilder-go: https://github.com/infracloudio/msbotbuilder-go. It shows
 how to create a simple bot that accepts input from the user and echoes it back.
 
-
-Run the example
+# Run the example
 
 Bring up a terminal and run. Set two variables for the session as APP_ID and APP_PASSWORD to the values of
 your BotFramework app_id and password. Then, run:
-		go run main.go
+
+	go run main.go
+
 This will start a server which will listen on port 3978
 
-
-Understanding the example
+# Understanding the example
 
 The program starts by creating a hanlder struct of type `activity.HandlerFuncs`.
 This struct contains defination for the `OnMessageFunc` field which is a treated as a callback by the library
@@ -23,16 +24,17 @@ on the respective event.
 		},
 	}
 
-
 A webserver is started with a hanlder passed the received payload to `adapter.ParseRequest`
 This methods authenticates the payload, parses the request and returns an Activity value.
+
 	activity, err := adapter.ParseRequest(ctx, req)
 
 The Activity is then passed to `adapter.ProcessActivity` with the hanlder created to process
 the activity as per the hanlder functions and send the response to the connector service.
+
 	err = adapter.ProcessActivity(ctx, activity, customHandler)
 
-In case of no error, this web responds with a 200 status
+# In case of no error, this web responds with a 200 status
 
 To expose this local IP outside your local network, a tool like ngrok can be used.
 
